113341464990108621ed2dbed73966b6cc391af2
mathieudad <43423223+mathieudad@users.noreply.github.com>
2020-04-15 20:44:56 +0200

    ajout de la fonctionnalité enchere

---
+++ traitementAjoutVente.php
@@ -0,0 +1,21 @@
+<?php 
+
+	//recuperer les données venant de la page HTML 
+	//le parametre de $_POST = "name" de <input> de votre page HTML 
+	$nom = isset($_POST["Nom"])? $_POST["Nom"] : ""; 
+	$description = isset($_POST["Description"])? $_POST["Description"] : ""; 
+	$categorie = isset($_POST["Categorie"])? $_POST["Categorie"] : ""; 
+	$prixDepart = isset($_POST["PrixDepart"])? $_POST["PrixDepart"] : ""; 
+	$prixAchatImmediat = isset($_POST["PrixAchatImmediat"])? $_POST["PrixAchatImmediat"] : ""; 
+	$typeVente = isset($_POST["TypeVente"])? $_POST["TypeVente"] : ""; 
+	$video = 
+
+
+
+function ajoutVente($id)
+{
+	
+
+}
+
+ ?>
