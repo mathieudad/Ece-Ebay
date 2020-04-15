8a6e9694a00fce9ded324fb1b5d4c54a735bd2f3
Todorovic Nikola <nikola.todorovic200@gmail.com>
2020-04-15 12:01:16 +0200

    separer

--- traitementFerraille.php
+++ traitementFerraille.php
@@ -11,34 +11,35 @@ $db_found = mysqli_select_db($db_handle, $database);
 if ($db_found) {
 	$sql = "SELECT * FROM vente WHERE Categorie = 'Ferraille ou Tresor';";
 	$result = mysqli_query($db_handle, $sql);
-			//regarder s'il y a de résultat 
-	if (mysqli_num_rows($result) == 0) { 
-			//le livre recherché n'existe pas 
-		echo "Pas d'objet trouvé"; } 
-		else { 
+			//regarder s'il y a de résultat
+	if (mysqli_num_rows($result) == 0) {
+			//le livre recherché n'existe pas
+		echo "Pas d'objet trouvé"; }
+		else {
 			while ($data = mysqli_fetch_assoc($result)) {
-				echo "Id: " . $data['IdVente'] . '<br>';
 				$IdVendeur = $data['IdVendeur'];
 				$sql2 = "SELECT Nom FROM vendeur WHERE IdVendeur=$IdVendeur";
 				$result2 = mysqli_query($db_handle, $sql2);
 				$data2 = mysqli_fetch_assoc($result2);
 				$cheminPhoto = $data['Photo'];
-				echo "<img src ='$cheminPhoto'>";
-				echo "Nom:" . $data2['Nom'] . '<br>';
-				echo "Description: " . $data['Description'] . '<br>';
-				echo "Categorie: " . $data['Categorie'] . '<br>';
-				echo "Prix de depart: " . $data['PrixDepart'] . '<br>';
-				echo "Prix achat immediat :" . $data['PrixAchatImmediat'] . '<br>';
-				echo "Type de vente: " . $data['TypeVente'] . '<br>';
-				echo "Date d'ajout :" . $data['DateAjout'].'<br>';
+				echo '<li class="list-group-item">';
+				echo '<div class="media align-items-lg-center flex-column flex-lg-row p-3">';
+				echo '<div class="media-body order-2 order-lg-1">';
+				echo '<h5 class="mt-0 font-weight-bold mb-2">'.$data['Nom'].'</h5>';
+				echo '<p class="font-italic text-muted mb-0 small">'.$data['Description'].'</p>';
+				echo '<div class="d-flex align-items-center justify-content-between mt-1">';
+				echo '<h6 class="font-weight-bold my-2">'.$data['PrixAchatImmediat'].'€</h6></div>';
+				echo"</div><img src='$cheminPhoto'";
+				echo 'alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2"></div>  </li>';
+
 			}
 		}
-     	//end while
-	}else 
-	{ 
-		echo "Database not found"; 
-	} 
-	//fermer la connexion 
+			//end while
+	}else
+	{
+		echo "Database not found";
+	}
+	//fermer la connexion
 	mysqli_close($db_handle);    //end if
 
 	?>
